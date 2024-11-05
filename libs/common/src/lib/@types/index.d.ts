9c7b14f7c19ee3b87859d481d7e4a4facdb1ce33
tmphu <mr.phutran@gmail.com>
2024-11-05 22:33:03 +0700

    update vercel

--- libs/common/src/lib/@types/index.d.ts
+++ libs/common/src/lib/@types/index.d.ts
@@ -1,6 +1,7 @@
 /// <reference types="vite/client" />
 
 declare module '*.svg?react' {
+  import * as React from 'react';
   const content: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
   export default content;
 }
