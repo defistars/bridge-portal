a87a6eb7107eb622adb0432737536638d353793a
tmphu <mr.phutran@gmail.com>
2024-11-17 11:57:18 +0700

    redesign landing page

--- apps/bridge-portal/src/app/app.tsx
+++ apps/bridge-portal/src/app/app.tsx
@@ -1,15 +1,8 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { WagmiProvider } from 'wagmi';
 import { config } from '@bridge-portal/shared';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import { HomePage } from './Homepage';
+import { HomePage } from './pages/Homepage';
 import theme from '@bridge-portal/styles/theme';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Transfer } from './pages/Transfer';
@@ -18,11 +11,7 @@ import MainLayout from './layout/Layout';
 const router = createBrowserRouter([
   {
     path: '/',
-    element: (
-      <MainLayout>
-        <HomePage />
-      </MainLayout>
-    ),
+    element: <HomePage />,
   },
   {
     path: 'bridge',
